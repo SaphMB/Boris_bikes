7ec0896644a949de4b135352d3b33e1a504986d9
antoinedoolaeghe <antoinedoolaeghe@gmail.com>
2017-07-04 15:22:53 +0100

    created method release_bike and one linear synthax rspec

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,6 +1,4 @@
 require 'docking_station.rb'
 describe DockingStation do
-  it 'respond to release bile' do
-    expect(DockingStation).respond_to(release_bike)
-  end
+  it {is_expected.to respond_to :release_bike}
 end
