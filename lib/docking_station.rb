9ea9ee63b9857408aaf4dcc95177d9d01e95c484
antoinedoolaeghe <antoinedoolaeghe@gmail.com>
2017-07-04 17:12:07 +0100

    Added spec on Bike.working?

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,9 +1,12 @@
 class DockingStation
+  station = DockingStation.new
   def release_bike
+    Bike.new
   end
 end
 
 class Bike
   def working?
+    true
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,4 +1,10 @@
 require 'docking_station.rb'
 describe DockingStation do
   it {is_expected.to respond_to :release_bike}
+
+  it 'relese bike from station' do
+  station = DockingStation.new
+  bike = station.release_bike
+  expect(bike.working?).to be_truthy
+  end
 end
