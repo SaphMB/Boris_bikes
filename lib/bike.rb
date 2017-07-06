f900c5b07a222abc57c9f204a94872d15260b88f
Alison Woodman <aliwoodman@gmail.com>
2017-07-06 15:00:45 +0100

    report_broken method added and tested

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,5 +1,16 @@
 class Bike
+
+attr_accessor :working
+
+  def initialize
+    @working = true
+  end
+
   def working?
-    true
+    working
+  end
+
+  def report_broken
+    self.working = false
   end
 end
--- spec/Bike_spec.rb
+++ spec/Bike_spec.rb
@@ -8,4 +8,8 @@ it 'is bike working?' do
   expect(bike.working?).to be_truthy
 end
 
+it 'user can report bike broken' do
+  bike.report_broken
+  expect(bike.working?).to eq false
+end
 end
